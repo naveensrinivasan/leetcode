31ae4ab7f822ceda638a10b7360e5a4188917310
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-07-25 16:04:05 +0000

    Sync LeetCode submission - Find All Anagrams in a String (golang)

--- problems/find_all_anagrams_in_a_string/solution.go
+++ problems/find_all_anagrams_in_a_string/solution.go
@@ -1,28 +1,39 @@
-
-func isAnagram(s string, t string) bool {
-	if len(s) != len(t){
-		return false
-	}
-	x := make([]int,26)
-	for i := 0; i < len(s); i++ {
-		x[s[i]-97]++
-		x[t[i]-97]--
-	}
-	for _, i2 := range x {
-		if i2!=0{
+func findAnagrams(s string, p string) []int {
+	/*
+		O(n)
+		Logic for figuring out the anagram
+		Map with every char
+			- Increment the counter from the original word
+			- Decrement the counter from the compared string
+			- if they aren't same return false
+		Sliding window
+		- Loop for the original word from 0 to (len(s) - len(p))
+	*/
+	anagram := func(a, b string) bool {
+		if len(a) != len(b) {
 			return false
 		}
+		m := make([]int, 26)
+		for i, _ := range a {
+			m[a[i]-97]++
+			m[b[i]-97]--
+		}
+		for _, i2 := range m {
+			if i2 != 0 {
+				return false
+			}
+		}
+		return true
 	}
-	return true
-}
-
-func findAnagrams(s string, p string) []int {
-	var x []int
-	l := len(p)
-	for i := 0; i <= len(s)-l; i++ {
-		if isAnagram(s[i:i+l],p){
-			x = append(x,i)
+	var result []int
+	if len(s) == 0 || len(p) == 0 {
+		return nil
+	}
+	//Sliding window
+	for i := 0; i <= (len(s) - len(p)); i++ {
+		if anagram(s[i:i+len(p)], p) {
+			result = append(result, i)
 		}
 	}
-	return x
+	return result
 }
