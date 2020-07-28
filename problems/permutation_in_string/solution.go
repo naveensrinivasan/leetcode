90188a0a0031d17169e79e889c629a874e06cf5a
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-07-28 17:39:04 +0000

    Sync LeetCode submission - Permutation in String (golang)

--- problems/permutation_in_string/solution.go
+++ problems/permutation_in_string/solution.go
@@ -1,28 +1,20 @@
-func checkInclusion(s1, s2 string) bool {
-	if len(s1) > len(s2) {
-		return false
-	}
-	for i := 0; i < (len(s2)-len(s1))+1; i++ {
-		if ArePermutations(s1, s2[i:i+len(s1)]) {
+func checkInclusion(s1 string, s2 string) bool {
+	for i := 0; i <= len(s2)-len(s1); i++ {
+		if isthereasubstring(s1, s2[i:len(s1)+i]) {
 			return true
 		}
 	}
 	return false
 }
 
-
-
-func ArePermutations(s1, s2 string)bool  {
-	if len(s1) != len(s2){
-		return false
+func isthereasubstring(s1, s2 string) bool {
+	var alphabetCounter [26]rune
+	for i, i2 := range s1 {
+		alphabetCounter[i2-97]++
+		alphabetCounter[s2[i]-97]--
 	}
-	m := make(map[uint8]int)
-	for i:= 0;i<len(s1);i++ {
-			m[s1[i]-97]++
-			m[s2[i]-97]--
-	}
-	for _,v := range m{
-		if v > 0{
+	for _, i2 := range alphabetCounter {
+		if i2 != 0 {
 			return false
 		}
 	}
