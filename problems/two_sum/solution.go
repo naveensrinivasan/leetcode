270c59d628bfb43e543dcbe69fac091b2cfe695b
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-07-29 18:41:05 +0000

    Sync LeetCode submission - Two Sum (golang)

--- problems/two_sum/solution.go
+++ problems/two_sum/solution.go
@@ -1,12 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    m := make(map[int]int)
-    
-    for i,v := range nums{
-        potentialMatch := target - v
-        if location, ok := m[potentialMatch];ok{
-            return []int{i,location}
-        }
-        m[v] = i
-    }
-    return nil
+	m := make(map[int]int)
+	for i, v := range nums {
+		diff := target - v
+		if pos, ok := m[diff]; ok {
+			return []int{pos, i}
+		}
+		m[v] = i
+	}
+	return nil
 }
