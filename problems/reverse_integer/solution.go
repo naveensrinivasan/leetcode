8ae9ae0bf7d93c13554e77cf22b018a747e37c24
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-08-04 21:36:30 +0000

    Sync LeetCode submission - Reverse Integer (golang)

--- problems/reverse_integer/solution.go
+++ problems/reverse_integer/solution.go
@@ -1,12 +1,15 @@
+
+
 func reverse(x int) int {
-	pop := 0
+    	pop := 0
 	reversed := 0
 	for x != 0 {
 		pop = x % 10
-		reversed = reversed*10 + pop
+		// division result
 		x /= 10
+		reversed = (reversed * 10) + pop
 	}
-	if math.MaxInt32 < reversed || reversed < math.MinInt32 {
+    if math.MaxInt32 < reversed || reversed < math.MinInt32 {
 		return 0
 	}
 	return reversed
