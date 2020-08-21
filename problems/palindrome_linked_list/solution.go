e65c1341936709aba1249bad67d9cdf8105e4366
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-08-21 14:39:41 +0000

    Sync LeetCode submission - Palindrome Linked List (golang)

---
+++ problems/palindrome_linked_list/solution.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome(head *ListNode) bool {
+	s := []int{}
+	for head != nil {
+		s = append(s, head.Val)
+		head = head.Next
+	}
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
