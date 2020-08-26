7a67d58653479a9c09b29a5f755f44eb204c7c10
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-08-26 11:44:55 +0000

    Sync LeetCode submission - Add Two Numbers (golang)

--- problems/add_two_numbers/solution.go
+++ problems/add_two_numbers/solution.go
@@ -6,38 +6,33 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	carry := 0
-	var cur, prev, result *ListNode
+	remainder, carry := 0, 0
+	var prev, result *ListNode
 	for l1 != nil || l2 != nil {
-		var sum = 0
+		sum := carry
 
 		if l1 != nil {
 			sum += l1.Val
-		}
-		if l2 != nil {
-			sum += l2.Val
-		}
-
-		sum += carry
-		carry = sum / 10
-		if result == nil {
-			result = &ListNode{Val: sum % 10}
-			prev = result
-		} else {
-			cur = &ListNode{Val: sum % 10}
-			prev.Next, prev = cur, cur
-		}
-
-		if l1 != nil {
 			l1 = l1.Next
 		}
+
 		if l2 != nil {
+			sum += l2.Val
 			l2 = l2.Next
 		}
+
+		carry, remainder = sum/10, sum%10
+
+		if result == nil {
+			result = &ListNode{Val: remainder}
+			prev = result
+		} else {
+			prev.Next = &ListNode{Val: remainder}
+			prev = prev.Next
+		}
 	}
-	if carry != 0 {
-		cur = &ListNode{Val: carry}
-		prev.Next, prev = cur, cur
+	if carry > 0 {
+		prev.Next = &ListNode{Val: carry}
 	}
 	return result
 }
