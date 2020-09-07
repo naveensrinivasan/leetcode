3f3c79f7b3e5fa76affa970a3bbb9a78413e1c5f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-07 17:51:10 +0000

    Sync LeetCode submission - Palindrome Linked List (golang)

--- problems/palindrome_linked_list/solution.go
+++ problems/palindrome_linked_list/solution.go
@@ -6,18 +6,41 @@
  * }
  */
 func isPalindrome(head *ListNode) bool {
-	s := []int{}
-	for head != nil {
-		s = append(s, head.Val)
-		head = head.Next
+	if head == nil {
+		return true
 	}
-	left, right := 0, len(s)-1
-	for left < right {
-		if s[left] != s[right] {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	if slow.Next == nil {
+		return true
+	}
+
+	second := reverseLList(slow.Next)
+	cur := head
+	for cur != nil && second != nil {
+		if cur.Val != second.Val {
 			return false
 		}
-		left++
-		right--
+		cur = cur.Next
+		second = second.Next
 	}
 	return true
+}
+
+func reverseLList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next, prev, cur = prev, cur, next
+	}
+
+	head.Next = nil
+	return prev
 }
