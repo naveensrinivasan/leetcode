717bb24160d5a5f65f043670b4d1a39cf5ac292a
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-15 22:42:29 +0000

    Sync LeetCode submission - Word Pattern (golang)

--- problems/word_pattern/solution.go
+++ problems/word_pattern/solution.go
@@ -9,8 +9,8 @@ func wordPattern(pattern string, s string) bool {
 	h := make(map[string]string)
 
 	for i, c := range chars {
-		if value, ok := h[c]; !ok {
-			for _, v := range h {
+		if value, ok := h[c]; !ok { // checks for if the key exists
+			for _, v := range h { // checks for duplicate value
 				if v == words[i] {
 					return false
 				}
