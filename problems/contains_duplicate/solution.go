134760ad61a2f839c17c0c522112d9da6a09868e
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-17 21:30:11 +0000

    Sync LeetCode submission - Contains Duplicate (golang)

--- problems/contains_duplicate/solution.go
+++ problems/contains_duplicate/solution.go
@@ -1,13 +1,9 @@
-import "sort"
 func containsDuplicate(nums []int) bool {
-    if len(nums) ==1 {
-        return false
-    }
-    sort.Ints(nums)
-    for i:=1;i<len(nums);i++{
-        if nums[i] == nums[i-1]{
-            return true
-        }
-    }
-    return false
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] == nums[i] {
+			return true
+		}
+	}
+	return false
 }
