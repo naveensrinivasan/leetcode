2dac5eecd7d13c0031e3da3f53f12c1d6b24e2a4
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-18 22:18:16 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (golang)

---
+++ problems/guess_number_higher_or_lower/solution.go
@@ -0,0 +1,31 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+// func guess(num int) int {
+// 	return 1
+// }
+
+//1,2,3,4,5,6,7,8,9,10
+// 1+10 /2 = 5
+
+func guessNumber(n int) int {
+	low, high := 1, n
+	for low <= high {
+		mid := (low + high) / 2
+		switch guess(mid) {
+		case 0:
+			return mid
+		case -1:
+			high = mid - 1
+			break
+		case 1:
+			low = mid + 1
+		}
+	}
+	return n
+}
