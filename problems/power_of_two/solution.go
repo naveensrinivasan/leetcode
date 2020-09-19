4e73cfd1605f0c73d7a3da383b96d1fbf3cd91a4
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-19 01:30:58 +0000

    Sync LeetCode submission - Power of Two (golang)

--- problems/power_of_two/solution.go
+++ problems/power_of_two/solution.go
@@ -2,11 +2,16 @@ func isPowerOfTwo(n int) bool {
 	if n == 0 {
 		return false
 	}
-	for n != 1 {
+	return twosPower(n)
+}
+func twosPower(n int) bool {
+
+	if n != 1 {
 		if n%2 != 0 {
 			return false
 		}
 		n /= 2
+		return twosPower(n)
 	}
 	return true
 }
