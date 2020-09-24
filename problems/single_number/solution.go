39bb31ebfd1b6a5223d9a9ac9c598d07f7ff100c
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-24 21:26:27 +0000

    Sync LeetCode submission - Single Number (golang)

--- problems/single_number/solution.go
+++ problems/single_number/solution.go
@@ -1,16 +1,7 @@
 func singleNumber(nums []int) int {
-	m := make(map[int]bool)
-	sum := 0
-	hsum := 0
-	for _, item := range nums {
-		if _, exists := m[item]; !exists {
-			m[item] = true
-		}
-		sum += item
+	result := 0
+	for _, v := range nums {
+		result ^= v
 	}
-	for k, _ := range m {
-		hsum += k
-	}
-	return (2*(hsum) - sum)
-
+	return result
 }
