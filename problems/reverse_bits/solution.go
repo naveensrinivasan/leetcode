749a4f87b9e7ff6ba69c00ad04cc7c843e6f053a
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-24 22:28:29 +0000

    Sync LeetCode submission - Reverse Bits (golang)

--- problems/reverse_bits/solution.go
+++ problems/reverse_bits/solution.go
@@ -1,12 +1,10 @@
 func reverseBits(num uint32) uint32 {
-	output := uint32(0)
+	result := uint32(0)
 	for i := 0; i < 32; i++ {
-		/*
-		  add the last digit from num to the output by num & 1
-		  Left shift output for the next digit
-		*/
-		output = output<<1 + num&1
+		result = result << 1
+		result += num & 1
+		//result = result<<1 + num&1
 		num >>= 1
 	}
-	return output
+	return result
 }
