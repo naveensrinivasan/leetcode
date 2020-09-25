3a3825180b88883c4f3db0d9ece1f81c133cccd7
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-09-25 02:28:25 +0000

    Sync LeetCode submission - Perfect Number (golang)

---
+++ problems/perfect_number/solution.go
@@ -0,0 +1,12 @@
+func checkPerfectNumber(num int) bool {
+	if num < 1 {
+		return false
+	}
+	sum := 0
+	for i := 1; i <= num/2; i++ {
+		if num%i == 0 {
+			sum += i
+		}
+	}
+	return sum == num
+}
