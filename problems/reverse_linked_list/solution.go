24f593e4ecc081dd65a6f96492a245b448d1cdff
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-03-24 15:38:54 +0000

    Sync LeetCode submission - Reverse Linked List (golang)

--- problems/reverse_linked_list/solution.go
+++ problems/reverse_linked_list/solution.go
@@ -1,21 +1,21 @@
 /**
-* Definition for singly-linked list.
- type ListNode struct {
-*     Val int
-*     Next *ListNode
-* }
-*/
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func reverseList(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-	var prev *ListNode
-	cur := head
-	for cur != nil {
-		next := cur.Next
-		cur.Next, prev, cur = prev, cur, next
-	}
-
-	head.Next = nil
-	return prev
+    if head == nil{
+        return nil
+    }
+    var prev,next,cur *ListNode
+    cur = head
+    for cur != nil{
+        next = cur.Next
+        cur.Next = prev
+        prev = cur
+        cur = next
+    }
+    return prev
 }
