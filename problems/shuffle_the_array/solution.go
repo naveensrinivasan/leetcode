e43cf4268cd6b32c5dc3c8d1b3876e07b65b8b43
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-14 19:44:36 +0000

    Sync LeetCode submission - Shuffle the Array (golang)

--- problems/shuffle_the_array/solution.go
+++ problems/shuffle_the_array/solution.go
@@ -1,7 +1,8 @@
 func shuffle(nums []int, n int) []int {
-    var x []int
-	for i:=0;i<n;i++  {
-		x = append(x,nums[i],nums[i+(n)])
-	}
-	return x
+    result := []int{}
+    for i := 0;i<n;i++{
+        result = append(result,nums[i])
+        result = append(result,nums[i+n])
+    } 
+    return result
 }
