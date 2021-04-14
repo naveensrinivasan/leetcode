bbd4ef2b669a7e391be46546e9b81e73145e9a81
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-14 19:58:43 +0000

    Sync LeetCode submission - Subtract the Product and Sum of Digits of an Integer (golang)

---
+++ problems/subtract_the_product_and_sum_of_digits_of_an_integer/solution.go
@@ -0,0 +1,12 @@
+func subtractProductAndSum(n int) int {
+    
+    p := 1
+    s := 0
+    for (n> 0){
+        d := n %10
+        p *= d
+        s += d
+        n = n/10
+    }
+    return p -s
+}
