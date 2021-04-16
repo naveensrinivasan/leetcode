e9b99b43c6629f20172de4af27a07cc47d0c5757
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-16 15:04:14 +0000

    Sync LeetCode submission - Find K-Length Substrings With No Repeated Characters (golang)

--- problems/find_k-length_substrings_with_no_repeated_characters/solution.go
+++ problems/find_k-length_substrings_with_no_repeated_characters/solution.go
@@ -2,22 +2,40 @@ func numKLenSubstrNoRepeats(S string, K int) int {
     if K > len(S){
         return 0
     }
-    count := 0
     
-    for i:=0;i<len(S)-K+1;i++{
-        m := make(map[rune]int)
-        hasDupes := false  
-        for _ ,x := range S[i:i+K]{
-            m[x]++
-        } 
-        for k,_ := range m{
-            if m[k] > 1{
-                hasDupes = true
-                break
-            } 
+    count := 0
+    dupes := 0
+    
+    
+    m := make(map[string]int)
+    first := string(S[0])
+    
+    for i:=0;i<K;i++{
+        m[string(S[i])]++
+        if m[string(S[i])] == 2 {
+            dupes++
         }
-        if !hasDupes{
-            count++
+    }
+    if dupes == 0{
+        count++
+    }
+    
+    
+    for i:=K;i<len(S);i++{      
+        m[first]--
+        if (m[first] ==1){
+                dupes --
+        }
+        
+        first= string(S[i-K+1])
+        
+        m[string(S[i])]++
+        if m[string(S[i])] == 2 {
+            dupes++
+        }
+        
+        if dupes== 0{
+                count++
         }
     } 
     return count
