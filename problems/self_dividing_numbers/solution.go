6d0ac3ad51e554f73f7e1adfb5bd46717299e75f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-17 19:28:08 +0000

    Sync LeetCode submission - Self Dividing Numbers (golang)

--- problems/self_dividing_numbers/solution.go
+++ problems/self_dividing_numbers/solution.go
@@ -1,35 +1,38 @@
 func selfDividingNumbers(left int, right int) []int {
-	result := []int{}
-	for i := left; i <= right; i++ {
-		wasDivisble := true
-		x := separateDigit(i)
-		if len(x) == 0 {
-			continue
-		}
-		for _, d := range x {
-			if i%d != 0 {
-				wasDivisble = false
-				break
-			}
-		}
-		if wasDivisble {
-			result = append(result, i)
-		}
-
-	}
-	return result
+    result := []int{}
+    
+    for i:= left;i<=right;i++{
+        if i%10 == 0{
+                continue
+            }
+        wasDivisble := false
+        //fmt.Println(getdigits(i))
+        for _ , d := range  getdigits(i){
+            
+            if i%d != 0{
+                wasDivisble = false
+                break
+            } 
+            wasDivisble = true
+        }
+        if wasDivisble{
+            result = append(result, i)
+        }
+    }
+    return result
 }
-func separateDigit(n int) []int {
-	result := []int{}
-	if n == 1 {
-		return []int{1}
-	}
-	for n > 0 {
-		if n%10 == 0 {
-			return []int{}
-		}
-		result = append(result, n%10)
-		n /= 10
-	}
-	return result
+
+func getdigits(n int)[]int{
+    if n< 10{
+        return []int{n}
+    }
+    result := []int{}
+    for n > 0{
+        if n== 0 || n % 10 == 0{
+            return  []int{}
+        } 
+        result =append(result, n %10)    
+        n/=10
+    }
+    return result
 }
