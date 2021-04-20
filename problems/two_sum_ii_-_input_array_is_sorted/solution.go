c7811e827d9452058c241cd30365cbd77fa4db7b
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-20 14:43:42 +0000

    Sync LeetCode submission - Two Sum II - Input array is sorted (golang)

--- problems/two_sum_ii_-_input_array_is_sorted/solution.go
+++ problems/two_sum_ii_-_input_array_is_sorted/solution.go
@@ -1,14 +1,19 @@
 func twoSum(numbers []int, target int) []int {
-	left, right := 0, len(numbers)-1
-	for left < right {
-		sum := numbers[left] + numbers[right]
-		if sum == target {
-			return []int{left + 1, right + 1}
-		} else if sum > target {
-			right--
-		} else {
-			left++
-		}
-	}
-	return nil
+    
+    for index ,_ := range numbers{
+        
+    
+    left,right:= index, len(numbers)-1
+    for left<right && right < len(numbers){
+        sum := numbers[left] + numbers[right]
+        if sum == target{
+            return []int{left+1,right+1}
+        }else if sum > target{
+            right--
+        }else{
+            left++
+        }
+    }
+    }
+    return []int{}
 }
