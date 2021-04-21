ba7925b7b8b80c1e90921742a497d146a4655466
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-21 14:35:59 +0000

    Sync LeetCode submission - Reverse Vowels of a String (golang)

--- problems/reverse_vowels_of_a_string/solution.go
+++ problems/reverse_vowels_of_a_string/solution.go
@@ -1,32 +1,41 @@
 func reverseVowels(s string) string {
-	left, right := 0, len(s)-1
-	m := make(map[byte]rune)
-	m['a'] = 'a'
-	m['e'] = 'e'
-	m['i'] = 'i'
-	m['o'] = 'o'
-	m['u'] = 'u'
-	m['A'] = 'A'
-	m['E'] = 'E'
-	m['I'] = 'I'
-	m['O'] = 'O'
-	m['U'] = 'U'
-	result := make([]byte, len(s))
-	for left <= right {
-		if _, ok := m[s[left]]; !ok {
-			result[left] = s[left]
-			left++
-			continue
-		}
-		if _, ok := m[s[right]]; !ok {
-			result[right] = s[right]
-			right--
-			continue
-		}
-		result[left], result[right] = s[right], s[left]
-		left++
-		right--
+    l,r :=0,len(s)-1
+    result := []rune(s)
+    
+    for l<r {
+        foundLeft,foundRight := false,false
+        for l<=r  {
+            
+            if isVowel(rune(result[l])){
+                foundLeft = true
+                break
+            }
+            l++
+        }
+        
+        for r>=0{
+            if isVowel(rune(result[r])){
+                foundRight = true
+                break
+            }
+            r--
+        }
+        // if l == r || r==0{
+        //     return string(result)
+        // }
+        if foundLeft && foundRight {
+            result[l],result[r] = result[r],result[l]    
+        }
+        
+        l++
+        r--
+    }
+    return string(result)
+}
 
-	}
-	return string(result)
+func isVowel(r rune) bool{
+    if r=='a' || r == 'e' || r == 'i' || r== 'o' || r == 'u' || r=='A' || r == 'E' || r == 'I' || r== 'O' || r == 'U'{
+        return true
+    }
+    return false
 }
