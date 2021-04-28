74b41f98873a92d65852da1510209792c00b108f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-28 22:48:38 +0000

    Sync LeetCode submission - Backspace String Compare (golang)

---
+++ problems/backspace_string_compare/solution.go
@@ -0,0 +1,30 @@
+func backspaceCompare(s string, t string) bool {
+    left,right := 0,1
+    
+    for left <= right && right <= len(s)   {
+        if s[left] == '#'  && left != 0{
+            s = s[:left-1]+s[right:]
+            left=0
+            right=1
+        }else{
+            left++
+            right++
+        }
+    }
+        
+    left,right = 0,1
+    for left <= right && right <= len(t)   {
+        if t[left] == '#' && left != 0 {
+            t = t[:left-1]+t[right:]
+            left=0
+            right=1
+        }else{
+            left++
+            right++
+        }
+    }
+     
+    fmt.Println(s,t)
+    
+    return strings.Trim(s,"#")==strings.Trim(t,"#")
+}
