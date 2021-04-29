581885d3349efa7da5acc839730ece6c37bc4fb4
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-29 22:09:52 +0000

    Sync LeetCode submission - Find N Unique Integers Sum up to Zero (golang)

--- problems/find_n_unique_integers_sum_up_to_zero/solution.go
+++ problems/find_n_unique_integers_sum_up_to_zero/solution.go
@@ -1,10 +1,17 @@
 func sumZero(n int) []int {
-	result := []int{}
-	if n%2 == 1 {
-		result = append(result, 0)
-	}
-	for i := 1; i <= n/2; i++ {
-		result = append(result, i, -i)
-	}
-	return result
+    if n == 1{
+        return []int{0}
+    }
+    
+    r := n/2
+    start := r * -1
+    
+    result := []int{}
+    for i:= start;i<=r;i++{
+        if n%2== 0 && i == 0{
+            continue
+        }
+        result = append(result,i)
+    }
+    return result
 }
