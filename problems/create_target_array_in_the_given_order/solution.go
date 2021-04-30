7417543ea27310742a482f3c873480f0cc9690bb
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-04-30 12:35:03 +0000

    Sync LeetCode submission - Create Target Array in the Given Order (golang)

--- problems/create_target_array_in_the_given_order/solution.go
+++ problems/create_target_array_in_the_given_order/solution.go
@@ -2,7 +2,6 @@ func createTargetArray(nums []int, index []int) []int {
     a := []int{}
     for i := range nums{
         a = append(a[:index[i]],append([]int{nums[i]}, a[index[i]:]...)...)
-        
     }
     return a
 }
