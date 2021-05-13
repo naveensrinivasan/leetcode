5ea711b2f15087647f16403d67aeb157547102d5
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-05-13 20:01:46 +0000

    Sync LeetCode submission - Flipping an Image (golang)

---
+++ problems/flipping_an_image/solution.go
@@ -0,0 +1,33 @@
+func flipAndInvertImage(image [][]int) [][]int {
+    for _,j := range image{
+        l,r := 0,len(j)-1
+        
+    
+      
+        for l<r{
+            j[l],j[r] = j[r],j[l]
+            if j[l] == 0{
+                j[l] = 1
+            }else{
+                j[l]=0
+            }
+            if j[r] == 0{
+                j[r] = 1
+            }else{
+                j[r]=0
+            }
+             l++
+             r--
+        }
+        mid := (len(j)/2) 
+        fmt.Println(mid)
+        if len(j)%2 == 1{
+            if j[mid] == 0{
+                j[mid] = 1
+            }else{
+                j[mid] = 0
+            }
+        }
+    }
+    return image
+}
