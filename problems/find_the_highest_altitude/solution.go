6cc22acb8905303cd8cde7dcfa13e1b8a9d766ed
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-05-14 18:43:13 +0000

    Sync LeetCode submission - Find the Highest Altitude (golang)

--- problems/find_the_highest_altitude/solution.go
+++ problems/find_the_highest_altitude/solution.go
@@ -1,11 +1,7 @@
 func largestAltitude(gain []int) int {
     max,pos := 0,0
     for i:=0;i<len(gain);i++{
-        if gain[i] > 0{
-            pos = gain[i] + pos
-        }else{
-            pos = gain[i] + pos
-        }
+        pos = gain[i] + pos
         if pos > max{
             max = pos
         }
