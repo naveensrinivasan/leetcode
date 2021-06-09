de7c9ddfe9af34b928d56d6f6713b6f43f6cb61f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-09 16:53:34 +0000

    Sync LeetCode submission - Find and Replace Pattern (golang)

--- problems/find_and_replace_pattern/solution.go
+++ problems/find_and_replace_pattern/solution.go
@@ -8,25 +8,28 @@ func findAndReplacePattern(words []string, pattern string) []string {
             m[string(rune(i+97))]= string(rune(i+97))
         }
         
+        r := ""
         for i:=0;i<len(word);i++{
             if _,ok := w[string(word[i])];!ok{
                 if _,exists := m[string(pattern[i])];exists{
                     w[string(word[i])] = m[string(pattern[i])]
+                    r += string(pattern[i])
                     delete(m,string(pattern[i]))
                 }else{
                     break
                 }
+            }else{
+                r+= w[string(word[i])]
             }
         }
-        
-        r := ""
+        /*
         for i:=0;i<len(word);i++{
             if v,ok := w[string(word[i])];ok{
                 r += v
             }else{
                 break
             }
-        }
+        }*/
         if r == pattern{
             result = append(result,word)
         }
