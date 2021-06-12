fe5189c364586e2b605f8c392e57795841392b7a
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-12 14:31:44 +0000

    Sync LeetCode submission - Number of 1 Bits (golang)

--- problems/number_of_1_bits/solution.go
+++ problems/number_of_1_bits/solution.go
@@ -1,16 +1,10 @@
 func hammingWeight(num uint32) int {
-	result := 0
-	for i := 0; i < 32; i++ {
-		if num&1 == 1 {
-			result++
-		}
-		num >>= 1
-	}
-	// for num != 0 {
-	// 	if num&1 == 1 {
-	// 		result++
-	// 	}
-	// 	num >>= 1
-	// }
-	return result
+    result :=0
+    for num > 0{
+        if num &1 == 1{
+            result ++
+        }
+        num >>=1
+    }
+    return result
 }
