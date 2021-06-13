ae19513639339889f7da6c5de742b403cd873325
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-13 13:35:37 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number to Zero (golang)

--- problems/number_of_steps_to_reduce_a_number_to_zero/solution.go
+++ problems/number_of_steps_to_reduce_a_number_to_zero/solution.go
@@ -1,12 +1,15 @@
 func numberOfSteps(num int) int {
+    if num <1{
+        return 0
+    }
     counter := 0
     for num > 0{
         if num %2 == 0{
-            num /= 2
+            counter++
         }else{
-            num --
-        }  
-        counter++
+            counter+=2
+        }
+        num >>= 1
     }
-    return counter 
+    return counter -1
 }
