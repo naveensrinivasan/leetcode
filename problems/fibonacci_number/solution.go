cc16f2d7259a1545518af4e5ff4847876ba91e53
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-13 23:20:51 +0000

    Sync LeetCode submission - Fibonacci Number (golang)

--- problems/fibonacci_number/solution.go
+++ problems/fibonacci_number/solution.go
@@ -1,6 +1,9 @@
-func fib(N int) int {
-	if N == 0 || N == 1 {
-		return N
-	}
-	return fib(N-1) + fib(N-2)
+func fib(n int) int {
+    dp := make(map[int]int,n)
+    dp[0] = 0
+    dp[1] = 1
+    for i:=2;i<=n;i++{
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    return dp[n]
 }
