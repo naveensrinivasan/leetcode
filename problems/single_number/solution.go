e52a54c65bd6f02ffec85c364620500770a4b2b1
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-14 21:08:52 +0000

    Sync LeetCode submission - Single Number (golang)

--- problems/single_number/solution.go
+++ problems/single_number/solution.go
@@ -1,7 +1,7 @@
 func singleNumber(nums []int) int {
-	result := 0
-	for _, v := range nums {
-		result ^= v
-	}
-	return result
+    sum := 0
+    for _, i := range nums{
+        sum = sum ^ i
+    }
+    return sum
 }
