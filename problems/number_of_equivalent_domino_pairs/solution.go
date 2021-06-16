a5b62a9dc50255d2121e02456a39ea1389ddcfc8
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-16 21:21:56 +0000

    Sync LeetCode submission - Number of Equivalent Domino Pairs (golang)

---
+++ problems/number_of_equivalent_domino_pairs/solution.go
@@ -0,0 +1,21 @@
+func numEquivDominoPairs(dominoes [][]int) int {
+    type Key [2]int
+    result := 0
+    m:= make(map[Key]int)
+    for i:=0;i<len(dominoes);i++{
+        x := []int{dominoes[i][0],dominoes[i][1]}
+        sort.Ints(x)
+        f := Key{x[0],x[1]}
+        if _,ok := m[f];!ok{
+            m[f] = 0
+        }else{
+           m[f]++
+        }
+    }
+    for _,v := range m{
+        if v > 0{
+            result+= (v*(v+1))/2
+        }
+    }
+    return result
+}
