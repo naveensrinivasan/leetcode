452189e52443911fed4c8ddf88b6779daca31ad4
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-21 15:21:31 +0000

    Sync LeetCode submission - String Matching in an Array (golang)

--- problems/string_matching_in_an_array/solution.go
+++ problems/string_matching_in_an_array/solution.go
@@ -1,18 +1,17 @@
 func stringMatching(words []string) []string {
+    result := []string{}
+    
     sort.Slice(words,func(i,j int)bool{
         return len(words[i]) < len(words[j])
     })
-    m := make(map[string]string)
-    result := []string{}
+    
     for i:=0;i<len(words);i++{
         for j:= i+1;j<len(words);j++{
-            if _,ok := m[words[i]];!ok && len(words[i]) <= len(words[j]) && strings.Contains(words[j],words[i]) {
-                    m[words[i]] = words[i]
+            if strings.Contains(words[j],words[i]) {
+                    result = append(result,words[i])
+                    break
             }
         }
-    }
-    for k,_ := range m{
-        result = append(result,k)
     }
     return result
 }
