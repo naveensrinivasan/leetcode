54970ea0fc676ce3a627e298ead852598d22aedb
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-21 20:28:18 +0000

    Sync LeetCode submission - Sort an Array (golang)

---
+++ problems/sort_an_array/solution.go
@@ -0,0 +1,9 @@
+func sortArray(nums []int) []int {
+    for i:=0;i<len(nums);i++{
+        if i!=0 && nums[i] < nums[i-1]{
+            nums[i] , nums[i-1] = nums[i-1],nums[i]
+            i -= 2
+        }
+    }
+    return nums
+}
