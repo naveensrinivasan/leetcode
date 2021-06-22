dac3becaf9c8f4dc1ca08a27818c0a6d51ee6e4f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-22 16:52:18 +0000

    Sync LeetCode submission - Search Insert Position (golang)

---
+++ problems/search_insert_position/solution.go
@@ -0,0 +1,26 @@
+func searchInsert(nums []int, target int) int {
+    left,right,ans := 1,len(nums),0
+    if target > nums[len(nums)-1]{
+        return len(nums)
+    }
+    
+    if target <= nums[0]{
+        return 0
+    }
+    for left<= right{
+        mid := (left+right)/2
+        
+        if nums[mid] == target{
+            return mid
+        }
+        if nums[mid] < target{
+            left = mid +1
+            ans = left 
+        }else{
+            right = mid -1
+            ans = right + 1 
+        }
+    }
+    return ans
+  
+}
