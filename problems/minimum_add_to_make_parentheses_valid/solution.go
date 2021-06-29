51a0251d033a2eafc125efcd7dd5be894ec867cc
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-29 21:59:54 +0000

    Sync LeetCode submission - Minimum Add to Make Parentheses Valid (golang)

--- problems/minimum_add_to_make_parentheses_valid/solution.go
+++ problems/minimum_add_to_make_parentheses_valid/solution.go
@@ -1,35 +1,11 @@
-func minAddToMakeValid(S string) int {
-
-	result := 0
-	if len(S) == 0 {
-		return 0
-	}
-	makeValidParentheses := func(stack []string, character string, result *int) []string {
-
-		if len(stack) == 0 {
-			*result++
-			return stack
-		}
-		if stack[len(stack)-1] != character {
-			*result++
-		}
-		stack = stack[:len(stack)-1]
-		return stack
-	}
-	var stack []string
-	for _, char := range S {
-		switch string(char) {
-		case "{", "(", "[":
-			stack = append(stack, string(char))
-		case "]":
-			stack = makeValidParentheses(stack, "[", &result)
-		case "}":
-			stack = makeValidParentheses(stack, "{", &result)
-		case ")":
-			stack = makeValidParentheses(stack, "(", &result)
-		}
-
-	}
-
-	return result + len(stack)
+func minAddToMakeValid(s string) int {
+    stack :=[]string{}
+    for _, i := range strings.Split(s,""){
+        if i == ")" && len(stack) > 0 && stack[len(stack)-1] == "(" {
+            stack = stack[:len(stack)-1]
+            continue
+        }
+        stack = append(stack,string(i))
+    }
+    return len(stack)
 }
