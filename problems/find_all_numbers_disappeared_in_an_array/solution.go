c8ac25c832a9ec1850c232102db6a1727ab8e71b
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-06-29 15:43:47 +0000

    Sync LeetCode submission - Find All Numbers Disappeared in an Array (golang)

---
+++ problems/find_all_numbers_disappeared_in_an_array/solution.go
@@ -0,0 +1,15 @@
+func findDisappearedNumbers(nums []int) []int {
+    result := []int{}
+    m := make(map[int]int)
+    for _,i := range nums{
+        m[i]=i
+    }
+    for i:=1;i<=len(nums);i++{
+        if _,ok:=m[i];!ok{
+            result = append(result,i)
+        }
+    }
+    
+    
+    return result
+}
