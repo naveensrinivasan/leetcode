f40b7296a2971cc20ec45d541cfa4657fdb1b40f
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-02 22:51:29 +0000

    Sync LeetCode submission - Minimum Value to Get Positive Step by Step Sum (golang)

--- problems/minimum_value_to_get_positive_step_by_step_sum/solution.go
+++ problems/minimum_value_to_get_positive_step_by_step_sum/solution.go
@@ -1,20 +1,14 @@
 func minStartValue(nums []int) int {
-    sum := 0
-    i:= 1
-    for {
-        sum = i
-        wasBelowZero := false
-        for i:=0;i<len(nums);i++{
-            sum += nums[i]
-            if sum < 1{
-                break
-                wasBelowZero = true
-            }
+    sum := 1
+    min := 1
+    for i:=0;i<len(nums);i++{
+        sum += nums[i]   
+        if sum < min{
+            min = sum
         }
-        if !wasBelowZero && sum > 0{
-            return i
-        }
-        i++
     }
-    return 0
+    if min < 1{
+        return 2-min
+    }
+    return 1
 }
