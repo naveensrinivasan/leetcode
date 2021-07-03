f85b4759df58c9f189bcd8d5837d44ab29d88b8a
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-03 17:21:34 +0000

    Sync LeetCode submission - Armstrong Number (golang)

--- problems/armstrong_number/solution.go
+++ problems/armstrong_number/solution.go
@@ -1,18 +1,16 @@
-func isArmstrong(N int) bool {
-	d := splitDigits(N)
-	sum := 0
-	for _, v := range d {
-		sum += int(math.Pow(float64(v), float64(len(d))))
-	}
-	return sum == N
+func isArmstrong(n int) bool {
+    l := int(math.Log10(float64(n)))+1
+    x := n
+    sum :=0 
+    for x > 0{
+        sum+= pow(x%10,l)
+        x/=10
+    }
+    return sum==n
 }
 
-func splitDigits(N int) []int {
-	result := []int{}
-	for N > 0 {
-		x := N % 10
-		result = append(result, x)
-		N /= 10
-	}
-	return result
+func pow(x,y int)int{
+   return int(math.Pow(float64(x),float64(y)))
 }
+
+
