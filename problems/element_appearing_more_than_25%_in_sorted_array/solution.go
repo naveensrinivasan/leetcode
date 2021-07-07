e0216bdb303878d6f287feea8765d11841425a42
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-07 15:54:22 +0000

    Sync LeetCode submission - Element Appearing More Than 25% In Sorted Array (golang)

--- problems/element_appearing_more_than_25%_in_sorted_array/solution.go
+++ problems/element_appearing_more_than_25%_in_sorted_array/solution.go
@@ -2,18 +2,16 @@ func findSpecialInteger(arr []int) int {
     if len(arr) == 1{
         return arr[0]
     }
-    x, max,count,maxitem:= -1,0,0,-1
-    x = arr[0]
+    max,count,maxitem:= 0,0,-1
     for i :=1;i<len(arr);i++{
-        if arr[i] == x {
+        if arr[i] == arr[i-1] {
             count++
+            if count > max{
+                max = count
+                maxitem = arr[i]
+            }
         }else{
             count = 0
-        }
-        x = arr[i]
-        if count > max{
-            max = count
-            maxitem = arr[i]
         }
     }
     return maxitem
