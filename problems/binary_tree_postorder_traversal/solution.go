1d86d5d7aa187ef3793028af5a54524ffbb5d539
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-09 17:29:05 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (golang)

--- problems/binary_tree_postorder_traversal/solution.go
+++ problems/binary_tree_postorder_traversal/solution.go
@@ -7,29 +7,15 @@
  * }
  */
 func postorderTraversal(root *TreeNode) []int {
-    s := []*TreeNode{}
     nums := []int{}
-    if root == nil{
-        return []int{}
-    }
-    s = append(s,root)
-    
-    for len(s) != 0{
-        node := s[len(s)-1]
-        s = s[:len(s)-1]
-        
-        if node!= nil{
-            nums = append([]int{node.Val},nums...)    
-        }
-        if node.Left != nil{
-            s = append(s,node.Left)
-        }
-        if node.Right != nil{
-            s = append(s,node.Right)
-        }
-        
-              
-        
-    }
+    helper(root,&nums)
     return nums
+}
+func helper(root *TreeNode,g *[]int){
+    if root == nil{
+        return
+    }
+    *g = append([]int{root.Val},*g...)
+    helper(root.Right,g)
+    helper(root.Left,g)
 }
