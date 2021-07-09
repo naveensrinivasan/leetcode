5499eb36929975757589c6a9e9bb0a6a57f9ac12
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-09 17:15:44 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (golang)

--- problems/binary_tree_preorder_traversal/solution.go
+++ problems/binary_tree_preorder_traversal/solution.go
@@ -7,33 +7,15 @@
  * }
  */
 func preorderTraversal(root *TreeNode) []int {
-    s := []*TreeNode{}
-    nums := []int{}
-    if root == nil{
-        return []int{}
+    g := []int{}
+    helper(root,&g)
+    return g
+}
+func helper(root *TreeNode,result *[]int){
+    if root== nil{
+        return
     }
-    s = append(s,root)
-    
-    for len(s) != 0{
-        node := s[len(s)-1]
-        s = s[:len(s)-1]
-        //fmt.Println(node)
-        if node!= nil{
-            nums = append(nums,node.Val)    
-        }
-        
-        if node.Right != nil{
-            s = append(s,node.Right)
-        }
-        if node.Left != nil{
-            s = append(s,node.Left)
-        }
-        
-        
-        
-        
-        
-        
-    }
-    return nums
+    *result = append(*result,root.Val)
+    helper(root.Left,result)
+    helper(root.Right,result)
 }
