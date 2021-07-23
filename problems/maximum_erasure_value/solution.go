0f5f16535056422bc7043624ccb78d88705e79ea
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-23 21:49:12 +0000

    Sync LeetCode submission - Maximum Erasure Value (golang)

--- problems/maximum_erasure_value/solution.go
+++ problems/maximum_erasure_value/solution.go
@@ -16,32 +16,7 @@ func maximumUniqueSubarray(nums []int) int {
     return max
 }
 
-func calcSum(m map[int]int)int{
-    sum := 0
-    for i := range m{
-        sum+=i
-    }
-    return sum
-}
 
-func lengthOfLongestSubstring(s string) int {
-   a,b,max,mapsize := 0,0,0,0
-    m := make(map[byte]byte)
-    for (b<len(s)){
-        if _,ok := m[s[b]];!ok{
-            m[s[b]]=s[b]
-            b++
-            mapsize++
-            max = Max(max,mapsize)
-        }else{
-            delete(m,s[a])
-            a++
-            mapsize--
-        }
-    }
-    
-    return max
-}
 
 // Max returns the larger of x or y.
 func Max(x, y int) int {
