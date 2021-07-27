da851f131c68ff80b3f199270811af6de1911498
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-27 17:23:40 +0000

    Sync LeetCode submission - Subtree of Another Tree (golang)

---
+++ problems/subtree_of_another_tree/solution.go
@@ -0,0 +1,94 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+    s :=[]*TreeNode{}
+    s = append(s,root)
+    for len(s) > 0{
+        pop := s[0]
+        s = s[1:]
+        if pop  == nil{
+            continue
+        }
+        if pop.Val == subRoot.Val{
+            if isSameTree(pop,subRoot){
+                return true
+            }
+        }
+        s = append(s,pop.Left)
+        s = append(s,pop.Right)
+    }
+    return false
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil{
+        return true
+    }
+    if p == nil || q == nil{
+        return false
+    }
+    
+    firstList := []*TreeNode{}
+    secondList := []*TreeNode{}
+    
+    firstList = append(firstList,p)
+    secondList = append(secondList,q)
+    
+    for len(firstList) != 0 || len(secondList) != 0{
+        first := firstList[0]
+        firstList = firstList[1:]
+        
+        second := secondList[0]
+        secondList = secondList[1:]
+        
+        
+        f,s := -100000, -100000
+        if first == second{
+            continue
+        }
+        
+        
+        if first != nil{
+            f = first.Val
+        }
+        if second != nil{
+            s = second.Val
+        }
+        
+        if f != s {
+            return false
+        }
+        
+        if first.Left != nil{
+            firstList = append(firstList,first.Left)
+        }else{
+            firstList = append(firstList,nil)
+        }
+        
+        if first.Right != nil{
+            firstList = append(firstList,first.Right)
+        }else{
+            firstList = append(firstList,nil)
+        }
+        
+        if second.Left != nil{
+            secondList = append(secondList,second.Left)
+        }else{
+            secondList = append(secondList,nil)
+        }
+        
+        if second.Right != nil{
+            secondList = append(secondList,second.Right)
+        }else{
+            secondList = append(secondList,nil)
+        }
+        
+    }
+    return true
+}
