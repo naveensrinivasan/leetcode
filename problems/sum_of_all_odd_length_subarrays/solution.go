f6fabe211b92bf7c48f3d882d6035471f9770e75
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-31 16:27:22 +0000

    Sync LeetCode submission - Sum of All Odd Length Subarrays (golang)

--- problems/sum_of_all_odd_length_subarrays/solution.go
+++ problems/sum_of_all_odd_length_subarrays/solution.go
@@ -1,19 +1,28 @@
 func sumOddLengthSubarrays(arr []int) int {
     sum := 0
+    total := arr[0]
+    result := make([]int,len(arr))
+    result[0] = arr[0]
+    for i:=1;i<len(arr);i++{
+        total += arr[i]
+        result[i] = total
+    }
     //sliding window
     //1 , 3 ,5
     for i:= 1;i<=len(arr);i=i+2{
     window := i
         for i:=0;i<=len(arr)-window;i++{
-            sum+= sumAll(arr[i:i+window])
+            if window == 1{
+                sum += arr[i]
+            }else{
+                if i == 0{
+                    sum+= result[i+window-1]
+                }else{
+                    
+                    sum+= result[i+window-1] - result[i-1]
+                }
+            }
         }
-    }
-    return sum
-}
-func sumAll(x []int)int{
-    sum := 0
-    for _, i := range x{
-        sum+=i
     }
     return sum
 }
