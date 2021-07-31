3a6728e3929765a607e39d04df2cbfd4f26c3495
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-07-31 16:03:28 +0000

    Sync LeetCode submission - Running Sum of 1d Array (golang)

--- problems/running_sum_of_1d_array/solution.go
+++ problems/running_sum_of_1d_array/solution.go
@@ -1,9 +1,6 @@
-func runningSum(nums []int) []int {
-    result := []int{}
-    sum := 0
-    for _,x := range nums{
-        sum+=x
-        result = append(result,sum)
-    }  
-    return result
+func runningSum(arr []int) []int {
+    for i:=1;i<len(arr);i++{
+        arr[i] += arr[i-1]
+    }
+    return arr
 }
