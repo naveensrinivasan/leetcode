382958544ab2b39d76fd9658a85b8de8189d6daa
Naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-08-07 18:17:11 +0000

    Sync LeetCode submission - Three Divisors (golang)

---
+++ problems/three_divisors/solution.go
@@ -0,0 +1,37 @@
+func isThree(n int) bool {
+    p := []float64{2.0, 3.0, 5.0, 7.0, 11.0, 13.0, 17.0, 19.0, 23.0, 29.0, 31.0, 37.0, 41.0, 43.0, 47.0, 53.0, 59.0, 61.0, 67.0, 71.0, 73.0, 79.0, 83.0, 89.0, 97.0}
+    m := make(map[float64]bool)
+    for _,item := range p{
+        m[item] = true
+    }
+    x := math.Sqrt(float64(n))
+    _,ok :=m[x]
+    return ok
+}
+
+// Get all prime factors of a given number n
+func PrimeFactors(n int) (pfs []int) {
+	// Get the number of 2s that divide n
+	for n%2 == 0 {
+		pfs = append(pfs, 2)
+		n = n / 2
+	}
+
+	// n must be odd at this point. so we can skip one element
+	// (note i = i + 2)
+	for i := 3; i*i <= n; i = i + 2 {
+		// while i divides n, append i and divide n
+		for n%i == 0 {
+			pfs = append(pfs, i)
+			n = n / i
+		}
+	}
+
+	// This condition is to handle the case when n is a prime number
+	// greater than 2
+	if n > 2 {
+		pfs = append(pfs, n)
+	}
+
+	return
+}
